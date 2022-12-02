2a5922223f5c7499982b15c329559062b208bbbd
Saul <iojkgh88@gmail.com>
2022-12-03 00:04:07 +0900

    [#3] Enum값을 dto로 전달위해 public전환.

--- src/main/java/com/flab/quicktogether/project/domain/Role.java
+++ src/main/java/com/flab/quicktogether/project/domain/Role.java
@@ -1,25 +1,21 @@
 package com.flab.quicktogether.project.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
 
 @Getter
 @RequiredArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Role {
 
-    private Role availableRole;
+    private RoleType availableRoleType;
     private SkillStack availableSkills;
 
 }
 
 
-enum SkillStack {
-    JAVA, KOTLIN, SPRING, JPA,
-    JAVA_SCRIPT, NODE_JS, EXPREESS_JS, NEST_JS, REACT_JS, VUE_JS, SVELTE_JS, ANGULAR_JS,
-    PYTHON, DJANGO, FLASK,
 
-}
-enum RoleType {
-    PROJECT_MANAGER, DESIGNER, FRONTEND, BACKEND, DATABASE_ADMINISTRATION
-}
---
+++ src/main/java/com/flab/quicktogether/project/domain/RoleType.java
@@ -0,0 +1,5 @@
+package com.flab.quicktogether.project.domain;
+
+public enum RoleType {
+    PROJECT_MANAGER, DESIGNER, FRONTEND, BACKEND, DATABASE_ADMINISTRATION
+}
