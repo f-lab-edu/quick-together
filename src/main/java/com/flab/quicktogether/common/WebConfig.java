c7356a4a945177bb487e8a20c933168f5e684883
Saul <iojkgh88@gmail.com>
2023-01-03 14:47:40 +0900

    refactor : LoginCheckArgumentResolver 네이밍 변경
    이름착오로 멤버아이디를 불러올수있는 이름으로 리팩토링

--- src/main/java/com/flab/quicktogether/common/WebConfig.java
+++ src/main/java/com/flab/quicktogether/common/WebConfig.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new LoginCheckArgumentResolver());
+        resolvers.add(new LoginMemberIdArgumentResolver());
     }
 
     @Override
