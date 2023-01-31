2eaf123ca1164e7d5b5133ec7daf4403e575a190
Saul <iojkgh88@gmail.com>
2023-01-31 20:58:56 +0900

    feat : TimePlan에서 AvailablePlan과 Plan 분리
    Plan의 저장장소가 db에 국한되지 않으므로 TimePlan을 Aggregate Root로 두기 어려워
    분리

---
+++ src/main/java/com/flab/quicktogether/timeplan/presentation/controller/PlanController.java
@@ -0,0 +1,40 @@
+package com.flab.quicktogether.timeplan.presentation.controller;
+
+import com.flab.quicktogether.common.Login;
+import com.flab.quicktogether.timeplan.application.PlanService;
+import com.flab.quicktogether.timeplan.presentation.dto.PlanCreateRequestDto;
+import com.flab.quicktogether.timeplan.presentation.dto.PlanUpdateRequestDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class PlanController {
+
+    private final PlanService planService;
+
+    @RequestMapping(path = "/events", method = RequestMethod.POST)
+    public ResponseEntity addPlan(@Login Long loginMemberId,
+                                  @Valid @RequestBody PlanCreateRequestDto event, BindingResult bindingResult) {
+        planService.registerPlan(loginMemberId, event);
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(path = "/events/{eventId}", method = RequestMethod.PUT)
+    public ResponseEntity editEvent(@Login Long loginMemberId,
+                                    @PathVariable Long eventId,
+                                    @Valid @RequestBody PlanUpdateRequestDto planUpdateRequestDto,
+                                    BindingResult bindingResult) {
+        planService.modifyPlan(loginMemberId, eventId, planUpdateRequestDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(path = "/events/{eventId}", method = RequestMethod.DELETE)
+    public ResponseEntity removeEvent(@Login Long loginMemberId, @PathVariable Long eventId) {
+        planService.removePlan(loginMemberId, eventId);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/flab/quicktogether/timeplan/presentation/controller/WeeklyAvailablePlanController.java
@@ -0,0 +1,47 @@
+package com.flab.quicktogether.timeplan.presentation.controller;
+
+import com.flab.quicktogether.common.Login;
+import com.flab.quicktogether.timeplan.application.WeeklyAvailablePlanService;
+import com.flab.quicktogether.timeplan.presentation.dto.AvailablePlanCreateRequestDto;
+import com.flab.quicktogether.timeplan.presentation.dto.AvailablePlanGetDto;
+import com.flab.quicktogether.timeplan.presentation.dto.AvailablePlanRequestDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/available_plan")
+@RequiredArgsConstructor
+public class WeeklyAvailablePlanController {
+
+    private final WeeklyAvailablePlanService availableTimeService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<AvailablePlanGetDto> getTimePlan(@Login Long loginMemberId,
+                                                           @RequestParam String timezone) {
+        AvailablePlanGetDto timePlanGetRequestDto = availableTimeService.getAvailablePlan(loginMemberId, timezone);
+        return ResponseEntity.ok(timePlanGetRequestDto);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<HttpStatus> postTimePlan(@Login Long loginMemberId,
+                                                   @Valid @RequestBody AvailablePlanCreateRequestDto timePlanCreateRequestDto, BindingResult bindingResult) {
+        availableTimeService.registerWeeklyAvailableRoutine(loginMemberId, timePlanCreateRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @RequestMapping(path = "/routine", method = RequestMethod.PUT)
+    public ResponseEntity updateAbleRoutine(@Login Long loginMemberId,
+                                            @Valid @RequestBody List<AvailablePlanRequestDto> ableRoutinesRequestDtos) {
+        availableTimeService.updateAvailablePlan(loginMemberId, ableRoutinesRequestDtos);
+        return ResponseEntity.ok().build();
+    }
+
+
+}
