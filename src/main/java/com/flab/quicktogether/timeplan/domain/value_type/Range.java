be6314b57dfcb364ff486a7a7518da6d06a51623
Saul <iojkgh88@gmail.com>
2023-01-31 19:09:54 +0900

    feat : 범위 지정용 ValueType 개발
    1. TimeZone 적용으로 기존 LocalDate사용에서 LocalDateTime으로 변경
    
    2. 생성자 파라미터로 LocalDate를 받지만, utc기준으로 LocalDateTime으로 필드를 저장함.

---
+++ src/main/java/com/flab/quicktogether/timeplan/domain/value_type/Range.java
@@ -0,0 +1,57 @@
+package com.flab.quicktogether.timeplan.domain.value_type;
+
+import com.flab.quicktogether.timeplan.domain.exception.NotNaturalTimeOrderException;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+/**
+ * 범위를
+ */
+@Getter
+@EqualsAndHashCode
+@ToString
+public final class Range {
+    private final LocalDateTime startDateTime;
+    private final LocalDateTime endDateTime;
+
+    private Range(LocalDateTime startDateTime, LocalDateTime endDateTime) {
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+    }
+
+    public static Range asCommonTime(LocalDate startDate, LocalDate endDate, ZoneId zoneId) {
+        ZoneId utc = ZoneId.of("UTC");
+        verifyNaturalDateOrder(startDate, endDate);
+
+        LocalDateTime from = startDate
+                .atStartOfDay(zoneId)
+                .withZoneSameInstant(utc)
+                .toLocalDateTime();
+
+        LocalDateTime to = endDate
+                .atStartOfDay(zoneId)
+                .plusDays(1L)
+                .withZoneSameInstant(utc)
+                .toLocalDateTime();
+
+        return new Range(from, to);
+    }
+
+    public static Range asCommonTime(LocalDate startDate, LocalDate endDate) {
+        ZoneId utc = ZoneId.of("UTC");
+        return asCommonTime(startDate, endDate, utc);
+    }
+
+    private static void verifyNaturalDateOrder(LocalDate startDate, LocalDate endDate) {
+        if (startDate.compareTo(endDate) > 0) {
+            throw new NotNaturalTimeOrderException();
+        }
+    }
+
+
+}
