0170134482d3d7519f0c09ffef89e1695e3fdf0f
hong <ghdtmdwo123@naver.com>
2023-02-03 17:10:39 +0900

    fix: 프로젝트 입장 시 알림을 어드민으로 보내게 수정

--- src/main/java/com/flab/quicktogether/alarm/firebase/event/EnterEventHandler.java
+++ src/main/java/com/flab/quicktogether/alarm/firebase/event/EnterEventHandler.java
@@ -3,6 +3,9 @@ package com.flab.quicktogether.alarm.firebase.event;
 import com.flab.quicktogether.alarm.firebase.NotificationMessageProvider;
 import com.flab.quicktogether.alarm.firebase.FcmService;
 import com.flab.quicktogether.member.infrastructure.FcmTokenRepository;
+import com.flab.quicktogether.participant.domain.Participant;
+import com.flab.quicktogether.participant.exception.ParticipantNotFoundException;
+import com.flab.quicktogether.participant.infrastructure.ParticipantRepository;
 import com.flab.quicktogether.project.support.enter.event.ProjectEnterEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,14 +18,19 @@ import org.springframework.stereotype.Service;
 public class EnterEventHandler {
     private final FcmService fcmService;
     private final FcmTokenRepository fcmTokenRepository;
+    private final ParticipantRepository participantRepository;
+
 
     @EventListener(ProjectEnterEvent.class)
     public void handle(ProjectEnterEvent event) {
 
-        Long enteredMemberId = event.getEnteredMemberId();
+        Long projectId = event.getProjectId();
+
+        Participant participantAdmin = participantRepository.findAdminByProjectId(projectId)
+                .orElseThrow(ParticipantNotFoundException::new);
 
         // 프로젝트 어드민이 로그인상태인 경우 알림을 보냄
-        fcmTokenRepository.findByMemberId(enteredMemberId)
+        fcmTokenRepository.findByMemberId(participantAdmin.getMember().getId())
                 .ifPresentOrElse(fcmToken -> {
                             fcmService.sendAlarm(NotificationMessageProvider.enterMember(fcmToken.getToken()));
                         },
