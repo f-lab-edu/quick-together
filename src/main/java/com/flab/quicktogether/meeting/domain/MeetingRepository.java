66d9cea8f7dc393b44e6ee0531ebe960a6473d2f
Saul <iojkgh88@gmail.com>
2023-02-12 02:03:27 +0900

    feat : MeetingRepository 요청상태 조회기능 개발

--- src/main/java/com/flab/quicktogether/meeting/domain/MeetingRepository.java
+++ src/main/java/com/flab/quicktogether/meeting/domain/MeetingRepository.java
@@ -2,9 +2,13 @@ package com.flab.quicktogether.meeting.domain;
 
 import com.flab.quicktogether.meeting.domain.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
+
+    @Query("select m from Meeting m where m.meetingStatus = 'REQUESTED'")
+    List<Meeting> findMeetingsByMeetingStatusIsRequested();
 
 }
