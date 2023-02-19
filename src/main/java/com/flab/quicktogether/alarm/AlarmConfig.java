49f1dc58f03dec131ada117cf2ca90a1f7acf64e
hongseungjae <ghdtmdwo123@naver.com>
2023-02-19 17:50:32 +0900

    test: 빈주입

--- src/main/java/com/flab/quicktogether/alarm/AlarmConfig.java
+++ src/main/java/com/flab/quicktogether/alarm/AlarmConfig.java
@@ -1,6 +1,8 @@
 package com.flab.quicktogether.alarm;
 
 import com.flab.quicktogether.alarm.firebase.FcmAlarmSendService;
+import com.flab.quicktogether.alarm.firebase.FcmSimpleAlarmMessageService;
+import com.flab.quicktogether.alarm.service.AlarmMessageService;
 import com.flab.quicktogether.alarm.service.AlarmSendService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +13,11 @@ public class AlarmConfig {
     @Bean
     public AlarmSendService alarmSendService() {
         return new FcmAlarmSendService();
+    }
+
+    @Bean
+    public AlarmMessageService alarmMessageService() {
+        return new FcmSimpleAlarmMessageService();
     }
 
 
