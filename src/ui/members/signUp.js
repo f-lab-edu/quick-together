d034660eac16b6d1216382a1f880315bf3511279
Saul <iojkgh88@gmail.com>
2023-03-04 05:26:04 +0900

    fix: same-site 이슈해결
    경로지정을 localhost로 명시하여 쿠키가 정상으로 넘어오도록함.

--- src/ui/members/signUp.js
+++ src/ui/members/signUp.js
@@ -14,18 +14,18 @@ function signUp() {
     const data = JSON.stringify(memberRequest)
     console.log(data)
     const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:8080/members')
-    xhr.setRequestHeader('Content-type', 'application/json')
+    xhr.open('POST', 'http://localhost:8080/members');
+    xhr.setRequestHeader('Content-type', 'application/json');
     xhr.withCredentials = true;
     xhr.send(data)
 
     xhr.onreadystatechange = () => {
         if (xhr.readyState !== XMLHttpRequest.DONE) return;
         if (xhr.status === 201) {
-            login(memberName, password)
-            window.location.href = "/available-plans/available-plan-form.html"
+            login(memberName, password);
+            window.location.href = "http://localhost:5500/available-plans/available-plan-form.html"
         } else {
-            console.log(xhr.status, xhr.response)
+            console.log(xhr.status, xhr.response);
         }
 
 
@@ -33,6 +33,7 @@ function signUp() {
 
 
 }
+//
 function login(memberName, password) {
     const loginForm = {
         memberName: memberName,
@@ -42,15 +43,15 @@ function login(memberName, password) {
     const data = JSON.stringify(loginForm)
     const uri = "http://localhost:8080/login"
     let xhr = new XMLHttpRequest();
-    xhr.open('POST', uri)
-    xhr.setRequestHeader('Content-type', 'application/json')
-    xhr.withCredentials = true
+    xhr.open('POST', uri);
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.withCredentials = true;
     xhr.send(data)
     xhr.onreadystatechange = () => {
         if (xhr.readyState !== XMLHttpRequest.DONE) return;
 
         if (xhr.status === 200) {
-            //do nothing
+            console.log("로그인이 완료되었다.")
         } else {
             console.log(xhr.response, xhr.status)
         }
